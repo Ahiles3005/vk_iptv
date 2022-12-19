1843376504582b97903c13c80f7c36f759c3caff
Kamil Mugutdinov <mugukamil@gmail.com>
2022-12-20 00:24:39 +0300

    update parser

--- src/Assets/ChannelList.js
+++ src/Assets/ChannelList.js
@@ -1,25 +1,26 @@
-import {M3uParser} from "m3u-parser-generator";
-
+// import { M3uParser } from "m3u-parser-generator";
+import parser from "iptv-playlist-parser";
 
 export const fetchM3U = fetch(
-    "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8"
+  "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8"
 )
-    .then((res) => {
-        return res.body.pipeThrough(new TextDecoderStream()).getReader();
-    })
-    .then((r) => {
-        return r.read().then(({value, done}) => {
-            const countries = []
-            const playlist = M3uParser.parse(value);
-            playlist.medias.forEach((channel) => {
-                let country = channel.attributes['group-title'];
-                if (!countries.includes(country)) {
-                    countries.push(country)
-                }
-            })
-            return {
-                medias: playlist.medias,
-                countries: countries
-            };
-        });
+  .then((res) => {
+    return res.body.pipeThrough(new TextDecoderStream()).getReader();
+  })
+  .then((r) => {
+    return r.read().then(({ value, done }) => {
+      const countries = [];
+      const playlist = parser.parse(value);
+      console.log(playlist);
+      playlist.items.forEach((channel) => {
+        let country = channel.group.title;
+        if (!countries.includes(country)) {
+          countries.push(country);
+        }
+      });
+      return {
+        medias: playlist.items,
+        countries: countries,
+      };
     });
+  });
